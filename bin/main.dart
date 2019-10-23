94128546768e5764ab6ec9d5a3d8409066d63c6c
McLarenCollege <hello@mclarencollege.in>
2019-10-23 08:28:20 +0530

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,25 @@
+// Challenge 1
+// Write failing test cases for the next two challenges
+
+// Challenge 2
+// All Pairs that Sum to Target
+// Create a function that returns all pairs of numbers in an list that sum to a target.
+// Sort the pairs in ascending order with respect to the smaller number,
+// then order each pair in this order: [smaller, larger].
+// Examples
+// allPairs([2, 4, 5, 3], 7) ➞ [[2, 5], [3, 4]]
+/// 2 + 5 = 7, 3 + 4 = 7
+
+
+// Challenge 3
+// Almost Sorted Sequence
+// An almost-sorted sequence is a sequence that is strictly increasing
+// if you remove a single element from the list
+// (no more, no less). Write a function that returns true if an list is almost-sorted,
+// and false otherwise.
+/// For example, if you remove 80 from the first example,
+/// it is perfectly sorted in ascending order.
+// Examples
+// almostSorted([1, 3, 5, 9, 11, 80, 15, 33, 37, 41] ) ➞ true
+
+main() {}
