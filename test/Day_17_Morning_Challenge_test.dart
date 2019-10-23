a46412537c06842a4f107f075c1008270197c236
McLarenCollege <hello@mclarencollege.in>
2019-10-23 08:29:08 +0530

    updating folders

--- test/Day_17_Morning_Challenge_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:Day_17_Morning_Challenge/Day_17_Morning_Challenge.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
